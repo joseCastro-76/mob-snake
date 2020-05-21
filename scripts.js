8fa7998e829c7c3ca6944aec7cc01b147a0b70a9
joseCastro-76 <56211699+joseCastro-76@users.noreply.github.com>
2020-05-21 16:56:36 -0700

    Adds Snake movement. Adds snake body

--- scripts.js
+++ scripts.js
@@ -1,3 +1,80 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = 25;
+var y = 25;
+var dx = 25;
+var dy = 0;
+
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
+
+let snakeBody = [];
+let snakeLength = 4;
+
+document.addEventListener("keydown", keyDownHandler, false);
+
+function keyDownHandler(e) {
+    e.preventDefault();
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+    else if(e.key == "Up" || e.key == "ArrowUp") {
+        upPressed = true;
+    }
+    else if(e.key == "Down" || e.key == "ArrowDown") {
+        downPressed = true;
+    }
+}
+
+function drawSnake() {
+    snakeBody.push([x, y])
+    ctx.beginPath();
+    ctx.rect(x, y, 25, 25);
+    ctx.fillStyle = "#32822F";
+    ctx.fill();
+    ctx.closePath();
+    if (snakeBody.length > snakeLength) {
+        const itemToRemove = snakeBody.shift();
+        ctx.clearRect(itemToRemove[0], itemToRemove[1], 25, 25);
+    }
+}
+
+
+function draw() {
+    drawSnake();
+
+    if(rightPressed) {
+        dx = 25;
+        dy = 0;
+    } else if(leftPressed) {
+        dx = -25;
+        dy = 0;
+    } else if(upPressed) {
+        dx = 0;
+        dy = -25;
+    } else if(downPressed) {
+        dx = 0;
+        dy = 25;
+    }
+
+    rightPressed = false;
+    leftPressed = false;
+    upPressed = false;
+    downPressed = false;
+
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 200);
+
+
+
 // const canvas = {
 //     element: document.querySelector("canvas"),
 //     context: document.querySelector("canvas").getContext('2d')
@@ -32,34 +109,3 @@
 //     }
 //   }
 //   canvas.context.fill();
-
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-// var x = canvas.width/2;
-var x = 25;
-// var y = canvas.height-30;
-var y = 25;
-var dx = 0;
-var dy = 25;
-var rightPressed = false;
-var leftPressed = false;
-var upPressed = false;
-var downPressed = false;
-
-function drawBall() {
-    ctx.beginPath();
-    ctx.rect(x, y, 25, 25);
-    // ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = "#32822F";
-    ctx.fill();
-    ctx.closePath();
-}
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    x += dx;
-    y += dy;
-}
-
-setInterval(draw, 500);
